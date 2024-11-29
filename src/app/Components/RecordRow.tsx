d49443953e784a311bbaad3b644c95057e7f6ca0
berkbozok <berk_bozok@live.com>
2024-11-29 22:47:17 +0300

    test v4

--- src/app/Components/RecordRow.tsx
+++ src/app/Components/RecordRow.tsx
@@ -109,6 +109,7 @@ export default function RecordRow() {
               </>
             )}
           </div>
+          {/* Inputs */}
           <input
             type="text"
             placeholder="Name"
