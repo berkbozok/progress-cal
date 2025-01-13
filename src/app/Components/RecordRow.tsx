59b67ef09e594c9fd9328e7932c063ccc444650f
berkbozok <berk_bozok@live.com>
2025-01-12 20:53:24 -0500

    commented

--- src/app/Components/RecordRow.tsx
+++ src/app/Components/RecordRow.tsx
@@ -124,6 +124,7 @@ export default function RecordRow() {
     }
   }
 
+  // Updating row
   async function updateRow(updatedRow: Row) {
     try {
       const rowsDocRef = doc(db, "exercises", "rows");
