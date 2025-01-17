c0bcd7cc453639a8235992030f799fb6fa5310d5
berkbozok <berk_bozok@live.com>
2025-01-17 00:58:47 -0500

    deployment fix

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; // Import Firestore
 import { getStorage } from "firebase/storage"; // Import Storage
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAXG-zmFjuGrCYzqhVrbD6cXg2NRKRmy3g",
   authDomain: "progress-cal.firebaseapp.com",
   projectId: "progress-cal",
-  storageBucket: "progress-cal.appspot.com", // Corrected the storage bucket URL
+  storageBucket: "progress-cal.appspot.com",
   messagingSenderId: "694811475043",
   appId: "1:694811475043:web:8c04b4b6523efff55ecc56",
   measurementId: "G-CMM3WHED56",
@@ -17,8 +17,26 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // Initialize Firestore and Storage
 export const db = getFirestore(app); // Firestore instance
 export const storage = getStorage(app); // Storage instance
+
+// Initialize Analytics only in supported environments
+export let analytics = null; // Export as null by default
+
+if (typeof window !== "undefined") {
+  isSupported()
+    .then((supported) => {
+      if (supported) {
+        analytics = getAnalytics(app);
+        console.log("Firebase Analytics initialized.");
+      } else {
+        console.log("Firebase Analytics is not supported in this environment.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error initializing Firebase Analytics:", error);
+    });
+}
