1b8a59d461ee5138140be528fe73ef7b841c28ed
berkbozok <berk_bozok@live.com>
2025-01-24 15:13:20 -0500

    deployment fix

--- src/app/Components/SavedRow.tsx
+++ src/app/Components/SavedRow.tsx
@@ -48,7 +48,7 @@ const SavedRow: React.FC<Props> = ({ rows, onDelete, onUpdate }) => {
       setEditedRow(null); // Clear the edited row state
     }
   }
-
+  // return
   return (
     <div className="mt-4 space-y-4">
       {rows.map((row) => (
